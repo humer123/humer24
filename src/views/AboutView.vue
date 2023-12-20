80aa79c230de7ad7e8338ea25d05e1d26b82ede2
homer <homergodinez9@gmail.com>
2023-12-20 19:42:43 +0800

    12

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainLayout from '../components/MainLayout.vue';
+
+// Page Title of Views
+const pageTitle = 'About'
+</script>
+
+<template>
+  <MainLayout :pageTitle="pageTitle">
+    <section>
+      Add Content About You
+    </section>
+  </MainLayout>
+</template>
